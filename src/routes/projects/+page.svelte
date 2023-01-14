4e8f09499e088283ecdba7815c80fa012c53235e
Thomas Basham <Bashamtg@gmail.com>
2023-01-13 20:03:57 -0800

    added gallery topic logic

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,22 +1,117 @@
 <script>
   import ImageGallery from "../../components/ImageGallery.svelte";
 
+  class galleryTopic {
+    constructor(topic, gallery) {
+      this.topic = topic;
+      this.gallery = gallery;
+    }
+  }
+
+  let gallery = [
+    {
+      largeURL: "",
+      thumbnailURL: "",
+      width: 0,
+      height: 0,
+    },
+  ];
   const images = [
     {
       largeURL:
-        "https://cdn.photoswipe.com/photoswipe-demo-images/photos/1/img-2500.jpg",
+        "https://cdn.photoswipe.com/photoswipe-demo-images/photos/3/img-2500.jpg",
       thumbnailURL:
-        "https://cdn.photoswipe.com/photoswipe-demo-images/photos/1/img-200.jpg",
-      width: 1875,
-      height: 2500,
+        "https://cdn.photoswipe.com/photoswipe-demo-images/photos/3/img-200.jpg",
+      width: 2500,
+      height: 1666,
     },
     {
       largeURL:
-        "https://cdn.photoswipe.com/photoswipe-demo-images/photos/2/img-2500.jpg",
+        "https://cdn.photoswipe.com/photoswipe-demo-images/photos/3/img-2500.jpg",
       thumbnailURL:
-        "https://cdn.photoswipe.com/photoswipe-demo-images/photos/2/img-200.jpg",
-      width: 1669,
-      height: 2500,
+        "https://cdn.photoswipe.com/photoswipe-demo-images/photos/3/img-200.jpg",
+      width: 2500,
+      height: 1666,
+    },
+    {
+      largeURL:
+        "https://cdn.photoswipe.com/photoswipe-demo-images/photos/3/img-2500.jpg",
+      thumbnailURL:
+        "https://cdn.photoswipe.com/photoswipe-demo-images/photos/3/img-200.jpg",
+      width: 2500,
+      height: 1666,
+    },
+    {
+      largeURL:
+        "https://cdn.photoswipe.com/photoswipe-demo-images/photos/3/img-2500.jpg",
+      thumbnailURL:
+        "https://cdn.photoswipe.com/photoswipe-demo-images/photos/3/img-200.jpg",
+      width: 2500,
+      height: 1666,
+    },
+    {
+      largeURL:
+        "https://cdn.photoswipe.com/photoswipe-demo-images/photos/3/img-2500.jpg",
+      thumbnailURL:
+        "https://cdn.photoswipe.com/photoswipe-demo-images/photos/3/img-200.jpg",
+      width: 2500,
+      height: 1666,
+    },
+    {
+      largeURL:
+        "https://cdn.photoswipe.com/photoswipe-demo-images/photos/3/img-2500.jpg",
+      thumbnailURL:
+        "https://cdn.photoswipe.com/photoswipe-demo-images/photos/3/img-200.jpg",
+      width: 2500,
+      height: 1666,
+    },
+    {
+      largeURL:
+        "https://cdn.photoswipe.com/photoswipe-demo-images/photos/3/img-2500.jpg",
+      thumbnailURL:
+        "https://cdn.photoswipe.com/photoswipe-demo-images/photos/3/img-200.jpg",
+      width: 2500,
+      height: 1666,
+    },
+    {
+      largeURL:
+        "https://cdn.photoswipe.com/photoswipe-demo-images/photos/3/img-2500.jpg",
+      thumbnailURL:
+        "https://cdn.photoswipe.com/photoswipe-demo-images/photos/3/img-200.jpg",
+      width: 2500,
+      height: 1666,
+    },
+    {
+      largeURL:
+        "https://cdn.photoswipe.com/photoswipe-demo-images/photos/3/img-2500.jpg",
+      thumbnailURL:
+        "https://cdn.photoswipe.com/photoswipe-demo-images/photos/3/img-200.jpg",
+      width: 2500,
+      height: 1666,
+    },
+    {
+      largeURL:
+        "https://cdn.photoswipe.com/photoswipe-demo-images/photos/3/img-2500.jpg",
+      thumbnailURL:
+        "https://cdn.photoswipe.com/photoswipe-demo-images/photos/3/img-200.jpg",
+      width: 2500,
+      height: 1666,
+    },
+    {
+      largeURL:
+        "https://cdn.photoswipe.com/photoswipe-demo-images/photos/3/img-2500.jpg",
+      thumbnailURL:
+        "https://cdn.photoswipe.com/photoswipe-demo-images/photos/3/img-200.jpg",
+      width: 2500,
+      height: 1666,
+    },
+    {
+      largeURL:
+        "https://cdn.photoswipe.com/photoswipe-demo-images/photos/3/img-2500.jpg",
+      thumbnailURL:
+        "https://cdn.photoswipe.com/photoswipe-demo-images/photos/3/img-200.jpg",
+      width: 2500,
+      height: 1666,
     },
     {
       largeURL:
@@ -27,9 +122,27 @@
       height: 1666,
     },
   ];
+
+  const galleryTopics = [
+    new galleryTopic("closets", images),
+    new galleryTopic("sheds", images),
+    new galleryTopic("remodels", images),
+    new galleryTopic("cabinets", images),
+  ];
+  function setGallery(newGal) {
+    gallery = newGal;
+  }
 </script>
 
-<div class='container mt-5 text-center gallery-container'>
-
-  <ImageGallery galleryID="projects" {images}/>
+<div class="container mt-5 text-center gallery-container">
+  <div class="row gallery-slector">
+    {#each galleryTopics as topic}
+      <div on:click={() => setGallery(topic.gallery)} class="border col text-white text-uppercase">
+        <h1>{topic.topic}</h1>
+      </div>
+    {/each}
+  </div>
+</div>
+<div class="right-space">
+  <ImageGallery galleryID="projects" images={gallery} />
 </div>

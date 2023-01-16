3f819217234f85b070712501202e91dad83a2f0c
Thomas Basham <Bashamtg@gmail.com>
2023-01-16 12:31:31 -0800

    added contact info

---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+   let name = "Thomas Basham";
+   let phone = "253-258-2526";
+   let email = "bashamtg@gmail.com";
+   let location = "Renton, Washington";
+</script>
+
+<div>
+  <h1>Contact {name}</h1>
+  <p>Phone: {phone}</p>
+  <p>Email: <a href="mailto:{email}">{email}</a></p>
+  <p>Location: {location}</p>
+</div>

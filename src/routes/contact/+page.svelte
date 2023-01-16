152e8dd7b1ed184257790490c7e2ad716fe5f4c6
Thomas Basham <Bashamtg@gmail.com>
2023-01-16 12:50:30 -0800

    updated contact page

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,17 +1,17 @@
 <script>
-   let name = "Thomas Basham";
-   let phone = "253-258-2526";
-   let email = "bashamtg@gmail.com";
-   let location = "Renton, Washington";
+  let name = "Thomas Basham";
+  let phone = "253-258-2526";
+  let email = "bashamtg@gmail.com";
+  let location = "Renton, Washington";
 </script>
 
 <div class="container right-space pt-5">
   <h1 class="text-center">Contact {name}</h1>
   <div class="card">
-      <div class="card-body">
-          <p class="card-text">Phone: {phone}</p>
-          <p class="card-text">Email: <a href="mailto:{email}">{email}</a></p>
-          <p class="card-text">Location: {location}</p>
-      </div>
+    <div class="card-body">
+      <p class="card-text">Phone: {phone}</p>
+      <p class="card-text">Email: <a href="mailto:{email}">{email}</a></p>
+      <p class="card-text">Location: {location}</p>
+    </div>
   </div>
 </div>
